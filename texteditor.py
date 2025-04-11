0b9ce156c74919abbac09b4d0ea2c86854e8337d
Partypants16 <parthshiv08@gmail.com>
2025-04-11 22:29:07 +1000

    Making a window

---
+++ texteditor.py
@@ -0,0 +1,12 @@
+import sys
+v=sys.version
+
+if "2.7" in v:
+    from Tkinter import *
+elif "3.3" in v or "3.4" in v:
+    from tkinter import *
+else:
+    from tkinter import *
+root = Tk()
+
+root.mainloop()

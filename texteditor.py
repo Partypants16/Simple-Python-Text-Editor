d84cae948bbaae8ba0f47ff5961e5b1311294745
Partypants16 <parthshiv08@gmail.com>
2025-04-12 12:41:37 +1000

    Font Changer

--- texteditor.py
+++ texteditor.py
@@ -22,6 +22,27 @@ def saveas():
     file1.write(t)
     file1.close()
 button=Button(root, text="Save", command=saveas)
-button.grid() 
+button.grid()
+
+def FontHelvetica():
+    global text
+    text.config(font="Helvetica")
+
+def FontCourier():
+    global text
+    text.config(font="Courier")
+
+font = Menubutton(root, text="Font")
+font.grid()
+font.menu = Menu(font, tearoff=0)
+font["menu"]=font.menu
+Helvetica = IntVar()
+arial = IntVar()
+times = IntVar()
+Courier = IntVar()
+font.menu.add_checkbutton(label="Courier", variable=Courier, 
+command=FontCourier)
+font.menu.add_checkbutton(label="Helvetica", variable=Helvetica,
+command=FontHelvetica)
 
 root.mainloop()
